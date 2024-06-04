0fd68dc890bee3d0f177c7353ed8114a9a3e7f74
DanielByamungu <56130261+DanielByamungu@users.noreply.github.com>
2024-06-04 12:09:45 -0400

    first commit from the Employee api

---
+++ src/main/java/com/example/employeedatabackend/EmployeeDataBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeedatabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDataBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeDataBackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/employeedatabackend/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.example.employeedatabackend.controller;
+
+import com.example.employeedatabackend.model.Employee;
+import com.example.employeedatabackend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    //create employee rest api
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
+    }
+
+}
